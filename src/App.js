e7ac27fe246fa22095218e2b92d5b459d59eab15
ThangPhan <thangphan@ThangPhannoMacBook-Pro.local>
2020-05-10 14:44:31 +0900

    Demo hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function Demo() {
+  const width = useWindowWidth();
+  const name = useFormInput("Marry");
+  const surname = useFormInput("Porter");
+
+  useEffect(() => {
+    document.title = name.value + " " + surname;
+  });
+
+  return (
+    <div className="form-container">
+      <div className="form-item">
+        <div>{name.value}</div>
+        <input {...name} />
+      </div>
+
+      <div className="form-item">
+        <div>{surname.value}</div>
+        <input {...surname} />
+      </div>
+
+      <div className="form-item">
+        <div>{width}</div>
+      </div>
+    </div>
+  );
+}
+
+function useFormInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
+  return {
+    value,
+    onChange: handleChange
+  };
+}
+
+function useWindowWidth() {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+
+  return width;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Demo />
     </div>
   );
 }
